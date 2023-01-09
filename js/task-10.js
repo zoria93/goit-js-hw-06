cd3b6a1d907842606c808f3c267479e7c78f8c0a
zoria93 <artemzora4@gmail.com>
2023-01-09 22:01:29 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,35 @@
+const inputNum = document.querySelector('#controls>input');
+const btnCreate = document.querySelector('button[data-create]');
+const btnDestroy = document.querySelector('button[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+btnCreate.addEventListener('click', () => {
+  let boxesToAdd = createBoxes(inputNum.value);
+  boxes.append(...boxesToAdd);
+});
+
+btnDestroy.addEventListener('click', () => {
+  destroyBoxes.call();
+});
+
+
+const destroyBoxes = () => {
+  boxes.innerHTML = '';
+};
+
+const createBoxes = amount => {
+  const elementsToAdd = []
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement('div');
+    div.style.height = `${30 + 10 * i}px`;
+    div.style.width = `${30 + 10 * i}px`;
+    div.style.background = getRandomHexColor();
+    elementsToAdd.push(div);
+  }
+  return elementsToAdd;
+  
+};
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`
+};
